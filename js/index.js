473e6c2af517e5e5f6d2281b632e2d8aa4f5c67e
786572258 <595171801@qq.com>
2016-04-13 14:09:12 +0800

    3333
    3333

--- js/index.js
+++ js/index.js
@@ -17,17 +17,7 @@ All methods returned accept two arguments, the source string and an options obje
 function get_beautify(js_beautify, css_beautify, html_beautify) {
     // the default is js
     var beautify = function(src, config) {
-        return js_beautify.js_beautify(src, config);
-    };
-
-    // short aliases
-    beautify.js = js_beautify.js_beautify;
-    beautify.css = css_beautify.css_beautify;
-    beautify.html = html_beautify.html_beautify;
-
-    // legacy aliases
-    beautify.js_beautify = js_beautify.js_beautify;
-    beautify.css_beautify = css_beautify.css_beautify;
+        return js_beautify.js_beautify(src, config.css_beautify;
     beautify.html_beautify = html_beautify.html_beautify;
 
     return beautify;
